51e916672e18a15ce783ecdc1768aec92d3ad18b
Julian <juliancejad@gmail.com>
2023-02-12 11:36:22 -0600

    Implement app service class with placeholder methods
    Implement app data handler with placeholder methods

--- src/Application/Services/AppService.java
+++ src/Application/Services/AppService.java
@@ -1,9 +1,35 @@
 package Application.Services;
 
+import Application.Domain.*;
+import Persistence.*;
 import java.util.*;
 
-import Persistence.AppDataHandler;
-
 public class AppService {
+    private AppDataHandler dataHandler;
 
+    public AppService( String connectionString ) {
+        dataHandler = new AppDataHandler( connectionString );
+    }
+
+    public boolean validatePhoneNumber( String phoneNumber ) {
+        return true;
+    }
+
+    public Iterable<String> generateWords( String phoneNumber ) {
+        return new LinkedList<String>();
+    }
+
+    public User getUser( String email ) {
+        return dataHandler.getUser( email );
+    }
+
+    public int storeUser( User user ) {
+        return dataHandler.storeUser( user );
+    }
+
+    public boolean existsPhoneNumber( String phoneNumber ) {
+        return dataHandler.existsPhoneNumber( phoneNumber );
+    }
+
+    
 }
--- src/Persistence/AppDataHandler.java
+++ src/Persistence/AppDataHandler.java
@@ -1,5 +1,48 @@
 package Persistence;
 
-public class AppDataHandler {
+import Application.Domain.*;
+import java.util.*;
 
+public class AppDataHandler {
+    private String connectionString;
+
+    public AppDataHandler( String connectionString ) {
+        this.connectionString = connectionString;
+    }
+
+    public User getUser( String email ) {
+        return new User();
+    }
+
+    public int storeUser( User user ) {
+        return 0;
+    }
+
+    public Company getCompany( int userId ) {
+        return new Company();
+    }
+
+    public int storeCompany( Company company ) {
+        return 0;
+    }
+
+    public boolean existsPhoneNumber( String phoneNumber ) {
+        return false;
+    }
+
+    public Map<String, Iterable<Word>> getPhoneNumbers( int companyId ) {
+        return new TreeMap<String, Iterable<Word>>();
+    }
+
+    public Iterable<Integer> storePhoneNumbers( Map<String, Iterable<Word>> phoneNumbers, int companyId ) {
+        return new LinkedList<Integer>();
+    }
+
+    public Iterable<Word> getWords( String phoneNumber ) {
+        return new LinkedList<Word>();
+    }
+
+    public Iterable<Integer> storeWords( Iterable<Word> words, String phoneNumber ) {
+        return new LinkedList<Integer>();
+    }
 }
