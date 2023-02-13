b40b3ce0eb237a96ca4740d492ae7536718807c6
Julian <juliancejad@gmail.com>
2023-02-13 11:57:48 -0600

    Add JUnit
    Create test suite for AppDataHandler
    
    Add SQLite jdbc driver (.jar)

---
+++ src/Tests/Unit/AppDataHandlerTests.java
@@ -0,0 +1,14 @@
+package Tests.Unit;
+
+import org.junit.jupiter.api.Test;
+
+import Application.Domain.Company;
+import Application.Domain.User;
+import Persistence.AppDataHandler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppDataHandlerTests {
+    private AppDataHandler appDataHandler = new AppDataHandler();
+
+}
