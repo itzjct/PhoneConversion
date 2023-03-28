9517ba68fdf26bfbaa6e1f782cb59b43aacc7ae2
Julian <juliancejad@gmail.com>
2023-03-27 20:18:09 -0500

    Partially implement App

--- src/Application/Domain/App.java
+++ src/Application/Domain/App.java
@@ -1,5 +1,294 @@
 package Application.Domain;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import Persistence.AppDataHandler;
+
 public class App {
-    
+
+    public final String HASH_ALGORITHM = "SHA-512";
+    public final int SALT_LEN = 32;
+    public final int NAME_CHAR_MAX = 32;
+    public final int EMAIL_CHAR_MAX = 128;
+    public final int PW_CHAR_MAX = 128;
+    public final int PW_CHAR_MIN = 6;
+
+    private User currentUser;
+    private String phoneNumber;
+    private boolean isUserLoggedIn;
+    private SecureRandom rng;
+    private Pattern emailPattern;
+    private AppDataHandler appDataHandler;
+
+    public App() {
+        appDataHandler = new AppDataHandler();
+
+        // Create random number generator
+        // Used to generate password salts
+        rng = new SecureRandom();
+
+        // Regex and pattern to check validity of email
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        emailPattern = Pattern.compile( emailRegex );
+    }
+
+    public void executeOption( int option ) {
+
+        // Login
+
+        // Register
+
+        // Generate words
+
+        // View company phone numbers
+
+        // Get words (approve words pt 1)
+
+        // Select approved (approve words pt 2)
+    }
+
+    public List<String> login( User user ) {
+        List<String> errors = new LinkedList<String>();
+
+        // Retrieve requested user from db
+        User userFromDb = new User();
+        try {
+            userFromDb = appDataHandler.getUser( user.getEmail() );
+        }
+        catch ( Exception ex ) {
+            errors.add( ex.getMessage() );
+            return errors;
+        }
+
+        // Given email does not correspond to user in db
+        if ( userFromDb == null ) {
+            errors.add( "Email not found" );
+            return errors;
+        }
+
+        // Hash password and validate
+        byte[] hashedPassword = hashPassword( user.getPasswordString(), userFromDb.getPasswordSalt() );
+        if ( !Arrays.equals( hashedPassword, userFromDb.getPassword() ) ) {
+            errors.add( "Invalid password" );
+            return errors;
+        }
+
+        // Clear user's text password (for security)
+        user.setPasswordString( null );
+
+        // Capture current user
+        // User.copy( user, userFromDb );
+        currentUser = userFromDb;
+
+        return errors;
+    }
+
+    public List<String> register( User user ) {
+
+        List<String> errors = new LinkedList<String>();
+
+        // Generate password salt and hash password
+        user.setPasswordSalt( getSalt() );
+        user.setPassword( hashPassword( user.getPasswordString(), user.getPasswordSalt() ) );
+
+        // Check if password hashing failed
+        if ( user.getPassword().length == 0 ) {
+            errors.add( "Hashing failed" );
+            return errors;
+        }
+
+        // Clear user's text password (for security)
+        user.setPasswordString( null );
+
+        // Submit company and user to db
+        try {
+
+            // Check if company exists
+            Company company = appDataHandler.getCompany( user.getCompany().getName() );
+
+            // If company does not exist in db then add it
+            if ( company == null ) {
+                int companyId = appDataHandler.storeCompany( user.getCompany() );
+                user.getCompany().setId( companyId );
+            }
+
+            // If it does exist, then create link between company and user
+            else {
+                user.setCompany( company );
+            }
+
+            // Add user
+            user.setId( appDataHandler.storeUser( user ) );
+        }
+        catch ( Exception ex ) {
+            errors.add( ex.getMessage() );
+            return errors;
+        }
+
+        // Capture current user
+        currentUser = user;
+
+        return errors;
+    }
+
+    public List<Word> generateWords( String phoneNumber ) {
+        return new LinkedList<Word>();
+    }
+
+    public List<String> getPhoneNumbers() {
+        return new LinkedList<String>();
+    }
+
+    public List<Word> getWords() {
+        return new LinkedList<Word>();
+    }
+
+    public boolean storeWords( List<Word> words ) {
+        return true;
+    }
+
+    public void logout() {
+        currentUser = null;
+        phoneNumber = null;
+        isUserLoggedIn = false;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public List<String> setPhoneNumber( String phoneNumber ) {
+
+        // Validate phone number
+
+        // Set phone number
+        this.phoneNumber = phoneNumber;
+
+        // If any, return error list
+        return new LinkedList<String>();
+    }
+
+    public boolean isUserLoggedIn() {
+        return isUserLoggedIn;
+    }
+
+    /*
+     * This method validates login user data
+     */
+    public List<String> validateLogin( User user ) {
+        List<String> errors = new LinkedList<String>();
+
+        if ( user.getEmail() == null || user.getEmail().equals( "" ) ) {
+            errors.add( "Email cannot be empty" );
+        }
+        if ( user.getPasswordString() == null || user.getPasswordString().equals( "" ) ) {
+            errors.add( "Password cannot be empty" );
+        }
+        if ( user.getPasswordString() != null && user.getPasswordString().length() < PW_CHAR_MIN ) {
+            errors.add( "Invalid password" );
+        }
+        if ( user.getPasswordString() != null && user.getPasswordString().length() > PW_CHAR_MAX ) {
+            errors.add( "Invalid password" );
+        }
+
+        return errors;
+    }
+
+    /*
+     * This method validates register user data
+     */
+    public List<String> validateUser( User user ) {
+        List<String> errors = new LinkedList<String>();
+
+        if ( user.getFirstName() == null || user.getFirstName().equals( "" ) ) {
+            errors.add( "First name cannot be empty" );
+        }
+        if ( user.getFirstName() != null && user.getFirstName().length() > NAME_CHAR_MAX ) {
+            errors.add( "First name cannot exceed " + NAME_CHAR_MAX + " characters" );
+        }
+        if ( user.getLastName() == null || user.getLastName().equals( "" ) ) {
+            errors.add( "Last name cannot be empty" );
+        }
+        if ( user.getLastName() != null && user.getLastName().length() > NAME_CHAR_MAX ) {
+            errors.add( "Last name cannot exceed " + NAME_CHAR_MAX + " characters" );
+        }
+        if ( user.getEmail() == null || user.getEmail().equals( "" ) ) {
+            errors.add( "Email cannot be empty" );
+        }
+        if ( user.getEmail() != null && user.getEmail().length() > EMAIL_CHAR_MAX ) {
+            errors.add( "Email cannot exceed " + EMAIL_CHAR_MAX + " characters" );
+        }
+        if ( user.getEmail() != null && !isValidEmail( user.getEmail() ) ) {
+            errors.add( "Invalid email" );
+        }
+        if ( user.getPasswordString() == null || user.getPasswordString().equals( "" ) ) {
+            errors.add( "Password cannot be empty" );
+        }
+        if ( user.getPasswordString() != null && user.getPasswordString().length() < PW_CHAR_MIN ) {
+            errors.add( "Password must be at least " + PW_CHAR_MIN + " characters" );
+        }
+        if ( user.getPasswordString() != null && user.getPasswordString().length() > PW_CHAR_MAX ) {
+            errors.add( "Password cannot exceed " + PW_CHAR_MAX + " characters" );
+        }
+
+        return errors;
+    }
+
+    /*
+     * This method validates a given company
+     */
+    public List<String> validateCompany( Company company ) {
+        List<String> errors = new LinkedList<String>();
+
+        if ( company.getName() == null || company.getName().equals( "" ) ) {
+            errors.add( "Company name cannot be empty" );
+        }
+        if ( company.getName() == null || company.getName().equals( "" ) ) {
+            errors.add( "Company name cannot exceed " + NAME_CHAR_MAX + " characters" );
+        }
+
+        return errors;
+    }
+
+    /*
+     * This method validates a given email
+     */
+    private boolean isValidEmail( String email ) {
+        return emailPattern.matcher( email ).matches();
+    }
+
+    /*
+     * This method hashes a given password with a given salt
+     */
+    public byte[] hashPassword( String password, byte[] salt ) {
+        try {
+            MessageDigest md = MessageDigest.getInstance( HASH_ALGORITHM );
+            md.update( salt );
+            return md.digest( password.getBytes( StandardCharsets.UTF_8 ) );
+        }
+        catch ( Exception ex ) {
+            System.out.println( ex.getMessage() );
+            return new byte[0];
+        }
+    }
+
+    /*
+     * This method generates a random password salt
+     */
+    public byte[] getSalt() {
+        byte[] salt = new byte[SALT_LEN];
+        rng.nextBytes( salt );
+        return salt;
+    }
 }
