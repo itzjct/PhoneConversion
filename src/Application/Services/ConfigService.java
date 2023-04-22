5581aa8d85633c04a6d9997e06ca932d2add22fa
Julian <juliancejad@gmail.com>
2023-04-22 18:14:19 -0500

    Add documentation to ConfigService

--- src/Application/Services/ConfigService.java
+++ src/Application/Services/ConfigService.java
@@ -3,17 +3,39 @@ package Application.Services;
 import java.io.*;
 import java.util.*;
 
+/**
+ * ConfigService is the class responsible for parsing
+ * the configuration options from config file.
+ * 
+ * @author Julian Ceja
+ * @author Nathan Ha
+ * @author Jacob Osbourne
+ * @author Matt Munsinger
+ * @version 1.0
+ */
+
 public class ConfigService {
+
+    // Constant specifying config file name
+    public static final String FILE_NAME = "config.properties";
+
+    // Constants specifying name of config options
+    // within config file
     public static final String DB_BASE = "DB_BASE";
     public static final String DB_PATH = "DB_PATH";
     public static final String DICTIONARY_PATH = "DICTIONARY_PATH";
 
+    // Properties object used to access parsed
+    // config options
     private Properties properties;
 
+    /**
+     * This constructor is used to read config file
+     * and parse its properties
+     */
     public ConfigService() {
         try {
-            String configPath = "config.properties";
-            FileInputStream in = new FileInputStream( configPath );
+            FileInputStream in = new FileInputStream( FILE_NAME );
             properties = new Properties();
             properties.load( in );
         }
@@ -22,6 +44,11 @@ public class ConfigService {
         }
     }
 
+    /**
+     * This method gives access to properties object
+     * 
+     * @return A Properties object.
+     */
     public Properties getProperties() {
         return this.properties;
     }
