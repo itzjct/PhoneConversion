e1b8e048ea7c79cf91573c35063b921494b9f1be
Julian <juliancejad@gmail.com>
2023-04-28 18:59:37 -0500

    Add documentation to LoginView

--- src/Presentation/Views/LoginView.java
+++ src/Presentation/Views/LoginView.java
@@ -13,11 +13,25 @@ import javax.swing.border.EmptyBorder;
 import Application.Domain.*;
 import Presentation.*;
 
+/**
+ * LoginView is the class responsible for displaying
+ * the user login view.
+ * 
+ * @author Julian Ceja
+ * @author Nathan Ha
+ * @author Jacob Osbourne
+ * @author Matt Munsinger
+ * @version 1.0
+ */
+
 public class LoginView {
 
+    // Constants used to specify dimensions
+    // of this view
     private final int WIDTH = 400;
     private final int HEIGHT = 200;
 
+    // Declare and/or instantiate needed components
     App app;
     JFrame frame;
     JPanel errorPanel = new JPanel( new GridLayout( 0, 1 ) );
@@ -31,18 +45,30 @@ public class LoginView {
     JButton backBtn = new JButton( "Back" );
     JButton submitBtn = new JButton( "Submit" );
 
+    /**
+     * This constructor is used to load components
+     * and logic related to approved phrases view.
+     * 
+     * @param passedFrame A JFrame object to reutilize between views.
+     * @param passedApp   An App object to handle business logic.
+     */
     public LoginView( JFrame passedFrame, App passedApp ) {
+
+        // Store frame and app globally
         app = passedApp;
         frame = passedFrame;
 
+        // Center back and submit buttons
         backBtn.setAlignmentX( Component.CENTER_ALIGNMENT );
         submitBtn.setAlignmentX( Component.CENTER_ALIGNMENT );
 
+        // Panel used to store bottom buttons
         btnPanel.setMaximumSize( new Dimension( 200, AppGUI.LINE_HEIGHT ) );
         btnPanel.setBorder( new EmptyBorder( 10, 0, 0, 0 ) );
         btnPanel.add( backBtn );
         btnPanel.add( submitBtn );
 
+        // Panel used to store form components
         JPanel formPanel = new JPanel( new GridLayout( 2, 2, 5, 5 ) );
         formPanel.setMaximumSize( new Dimension( 500, AppGUI.LINE_HEIGHT * 2 ) );
         formPanel.add( emailLb );
@@ -50,38 +76,55 @@ public class LoginView {
         formPanel.add( passwordLb );
         formPanel.add( passwordTxt );
 
+        // Panel used to store main content components
         contentPanel.setLayout( new BoxLayout( contentPanel, BoxLayout.Y_AXIS ) );
         contentPanel.setAlignmentX( Component.CENTER_ALIGNMENT );
         contentPanel.setBorder( new EmptyBorder( 10, 0, 0, 0 ) );
         contentPanel.add( formPanel );
 
+        // Setup header component
         header.setAlignmentX( Container.CENTER_ALIGNMENT );
         header.setFont( new Font( "Arial", Font.BOLD, 14 ) );
 
+        // Hide error panel initally
         errorPanel.setVisible( false );
 
+        // Panel used to store all other panels
         Container mainPanel = frame.getContentPane();
         mainPanel.add( header );
         mainPanel.add( errorPanel );
         mainPanel.add( contentPanel );
         mainPanel.add( btnPanel );
 
-        addActionListeners();
+        // Add needed listeners
+        addListeners();
 
+        // Set up view dimensions
         frame.setSize( WIDTH, HEIGHT );
-        frame.setVisible( true );
     }
 
-    private void addActionListeners() {
+    /**
+     * This method is responsible for adding
+     * listeners to needed components.
+     */
+    private void addListeners() {
         backBtn.addActionListener( x -> onBackClick() );
         submitBtn.addActionListener( x -> onSubmitClick() );
     }
 
+    /**
+     * This method is used to perform
+     * logic when back button is clicked.
+     */
     private void onBackClick() {
         frame.getContentPane().removeAll();
         StartMenuView startMenu = new StartMenuView( frame, app );
     }
 
+    /**
+     * This method is used to perform
+     * logic when submit button is clicked.
+     */
     private void onSubmitClick() {
         clearErrorMessages();
         if ( login() ) {
@@ -90,17 +133,28 @@ public class LoginView {
         }
     }
 
+    /**
+     * This method is used to perform login
+     * related logic.
+     * 
+     * @return True if operation succeeded.
+     *         False otherwise.
+     */
     private boolean login() {
+
+        // Retrieve email and password strings
         User user = new User();
         user.setEmail( emailTxt.getText().toUpperCase().trim() );
         user.setPasswordString( String.valueOf( passwordTxt.getPassword() ).trim() );
 
+        // Validate login info
         List<String> errors = app.validateLogin( user );
         if ( errors.size() > 0 ) {
             displayErrorMessages( errors );
             return false;
         }
 
+        // Login user
         errors = app.login( user );
         if ( errors.size() > 0 ) {
             displayErrorMessages( errors );
@@ -110,6 +164,12 @@ public class LoginView {
         return true;
     }
 
+    /**
+     * This method is used to display errors within
+     * view.
+     * 
+     * @param errors A List of String.
+     */
     private void displayErrorMessages( List<String> errors ) {
         for ( String error : errors ) {
             JLabel label = new JLabel( error );
@@ -123,6 +183,10 @@ public class LoginView {
         errorPanel.setVisible( true );
     }
 
+    /**
+     * This method is used to clear displayed
+     * error messages.
+     */
     private void clearErrorMessages() {
         errorPanel.removeAll();
         errorPanel.setVisible( false );
